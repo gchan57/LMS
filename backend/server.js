fd96f1bbae5ae3c90b93bbea214f9ab26d0c2b86
Gunachandru <tgunachandru@gmail.com>
2026-05-19 22:43:31 +0530

    Connected MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express=require("express");
 const app=express();
+require("dotenv").config()
+const connectDB=require("./config/db");
+
+connectDB();
+
 app.use(express.json());//ability to read json from frontend
 app.get("/",(req,res)=>{
 res.send("Running succcess");
 });
-const PORT=5000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server Live at ${PORT} `);
 })
