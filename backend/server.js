623384825acc576dfa3edcbea7db20c95b7e9989
Gunachandru <tgunachandru@gmail.com>
2026-05-20 16:06:00 +0530

    Created Authentication Backend

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express=require("express");
 const app=express();
 require("dotenv").config()
 const connectDB=require("./config/db");
-
 connectDB();
+const authRoutes=require("./routes/authRoutes")
+
 
 app.use(express.json());//ability to read json from frontend
+
 app.get("/",(req,res)=>{
 res.send("Running succcess");
 });
+app.use("/api",authRoutes);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server Live at ${PORT} `);
